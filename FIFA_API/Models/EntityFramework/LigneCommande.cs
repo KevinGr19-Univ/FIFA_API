58f2b70abbd9de31c5cad4c476efbb666b17d78c
Noa GUILLOT <noa.guillot@etu.univ-smb.fr>
2024-03-11 16:56:57 +0100

    Model : Langue, LigneCommande, Nation, Pays Fait (à vérifier)

--- FIFA_API/Models/EntityFramework/LigneCommande.cs
+++ FIFA_API/Models/EntityFramework/LigneCommande.cs
@@ -1,7 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FIFA_API.Models.EntityFramework
 {
+	[Table("t_e_lignecommande_lco")]
 	public class LigneCommande
 	{
+		[Column("lco_id")]
+		[Key()]
+		public int Id { get; set; }
 
-	}
+		[Column("lco_idvariantecouleurproduit")]
+		[ForeignKey("")]
+		public int IdVarianteCouleurProduit { get; set; }
+
+        [Column("lco_idtailleproduit")]
+        [ForeignKey("")]
+        public int IdTailleProduit { get; set; }
+
+        [Column("lco_idcommande")]
+        [ForeignKey("")]
+        public int IdCommande { get; set; }
+
+		[Column("lco_quantite")]
+		public int Quantite { get; set; }
+
+        [Column("lco_prix", TypeName="Numeric(10,2)")]
+        public int Prix { get; set; }
+
+
+    }
 }
--- FIFA_API/Models/EntityFramework/Nation.cs
+++ FIFA_API/Models/EntityFramework/Nation.cs
@@ -1,7 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FIFA_API.Models.EntityFramework
 {
+	[Table("t_e_nation_ntn")]
 	public class Nation
 	{
+		[Column("ntn_id")]
+		[Key()]
+		public int Id { get; set; }
 
+		[Column("ntn_nom")]
+		[StringLength(100, ErrorMessage = "Le nom de la nation ne doit dépasser les 100 caractères")]
+		public string Nom { get; set; }
 	}
 }
