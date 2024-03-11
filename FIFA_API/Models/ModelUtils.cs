2bf6f80007630ce6d457c4ffbfcde683398dfefe
KevinGr19 <kevin.grandjean@etu.univ-smb.fr>
2024-03-11 17:02:35 +0100

    Codage ModelUtils

--- FIFA_API/Models/ModelUtils.cs
+++ FIFA_API/Models/ModelUtils.cs
@@ -2,6 +2,6 @@
 {
     public static class ModelUtils
     {
-        public const string REGEX_CODEPOSTAL = "^([0-9]{2}|2[AB])[0-9]{3}$";
+        public const string REGEX_CODEPOSTAL = "^([0-9]{2}|2[AB])[0-9]{3}$"; // A tester
     }
 }
