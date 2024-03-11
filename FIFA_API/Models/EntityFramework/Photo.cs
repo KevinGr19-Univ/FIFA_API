4cb05126c0d1b70ded732538c9cf83eabaf05275
Luxd4y <hidri.imene@etu.univ_smb.fr>
2024-03-11 17:10:21 +0100

    Models : Photo,Produit,TailleProduit, ThemeVote

--- FIFA_API/Models/EntityFramework/Photo.cs
+++ FIFA_API/Models/EntityFramework/Photo.cs
@@ -1,7 +1,25 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+
 namespace FIFA_API.Models.EntityFramework
 {
-	public class Photo
+	[Table("t_e_photo_pht")]
+	public partial class Photo
 	{
+        [Key]
+        [Column("pht_id")]
+        public int Id { get; set; }
 
-	}
+        [Column("pht_nom")]
+        [StringLength(50)]
+        public string Nom { get; set; }
+
+        [Column("pht_url")]
+        [StringLength(500)]
+        public string Url { get; set; }
+
+    }
 }
