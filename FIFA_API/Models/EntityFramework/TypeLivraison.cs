2e5b9f9b24ee7e741137b3d4f96d88e3de1e2ff3
Dylan BATTIG <battigd@iut-acy.local>
2024-03-11 17:09:02 +0100

    Model properties

--- FIFA_API/Models/EntityFramework/TypeLivraison.cs
+++ FIFA_API/Models/EntityFramework/TypeLivraison.cs
@@ -1,7 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FIFA_API.Models.EntityFramework
 {
-	public class TypeLivraison
+    [Table("t_e_typelivraison_tli")]
+    public partial class TypeLivraison
 	{
+		[Key]
+		[Column("tli_id")]
+		public int Id { get; set; }
 
-	}
+		[Column("tli_nom")]
+        [StringLength(50)]
+		public string Nom { get; set; }
+
+        [Column("tli_prix")]
+        [Precision(2)]
+        public double Prix { get; set; }
+
+        [Column("tli_maxbusinessdays")]
+        public int MaxBusinessDays { get; set; }
+    }
 }
--- FIFA_API/Models/EntityFramework/Utilisateur.cs
+++ FIFA_API/Models/EntityFramework/Utilisateur.cs
@@ -1,7 +1,62 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 namespace FIFA_API.Models.EntityFramework
 {
-	public class Utilisateur
+    [Table("t_e_utilisateur_utl")]
+	public partial class Utilisateur
 	{
+        [Key]
+        [Column("utl_id")]
+        public int Id { get; set; }
 
-	}
+        [Column("utl_idlangue")]
+        public int IdLangue { get; set; }
+
+        [Column("utl_idpays")]
+        public int IdPays { get; set; }
+
+        [Column("utl_idpaysfavori")]
+        public int IdPaysFavori { get; set; }
+
+        [Column("utl_stripeid")]
+        [StringLength(100)]
+        public string StripeId { get; set; }
+
+        [Column("utl_phone")]
+        [StringLength(50)]
+        public string Phone { get; set; }
+
+        [Column("utl_prenom")]
+        [StringLength(100)]
+        public string Prenom { get; set; }
+
+        [Column("utl_mail")]
+        [StringLength(100)]
+        public string Mail { get; set; }
+
+        [Column("utl_surnom")]
+        [StringLength(100)]
+        public string Surnom { get; set; }
+
+        [Column("utl_datenaissance")]
+        public DateTime DateNaissance { get; set; }
+
+        [Column("utl_motpasse")]
+        [StringLength(200)]
+        public string MotPasse { get; set; }
+
+        [Column("utl_role")]
+        [StringLength(50)]
+        public string Role { get; set; }
+
+        [Column("utl_lastconnection")]
+        public DateTime LastConnection { get; set; }
+
+        [Column("utl_emailverified")]
+        public DateTime EmailVerified { get; set; }
+
+        [Column("utl_doublefa")]
+        public bool DoubleFA { get; set; }
+    }
 }
