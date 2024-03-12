1332a081bd095cf879ba22ea7a2a378b67a23df2
KevinGr19 <kevingr19dev@gmail.com>
2024-03-12 16:59:28 +0100

    Ajout publications

---
+++ FIFA_API/Models/LE CODE FIRST !!!/Article.cs
@@ -0,0 +1,9 @@
+﻿using FIFA_API.Models.EntityFramework;
+
+namespace FIFA_API.Models.LE_CODE_FIRST____
+{
+    public class Article : Publication
+    {
+        public string TexteArticle { get; set; }
+    }
+}
---
+++ FIFA_API/Models/LE CODE FIRST !!!/Document.cs
@@ -0,0 +1,9 @@
+﻿using FIFA_API.Models.EntityFramework;
+
+namespace FIFA_API.Models.LE_CODE_FIRST____
+{
+    public class Document : Publication
+    {
+        public string UrlPdf { get; set; }
+    }
+}
