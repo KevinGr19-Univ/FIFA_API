d1256aef18b14f8c15452f94b35f3837db76247f
KevinGr19 <kevingr19dev@gmail.com>
2024-03-12 17:55:06 +0100

    Ajout stats dans Joueur

--- FIFA_API/Models/LE CODE FIRST !!!/Joueur.cs
+++ FIFA_API/Models/LE CODE FIRST !!!/Joueur.cs
@@ -5,6 +5,7 @@ namespace FIFA_API.Models.LE_CODE_FIRST____
 {
     public enum Pied { Droitier = 0, Gaucher = 1, Ambidextre = 2 }
     public enum Poste { Attaquant = 0, Defenseur = 1, GardienDeBut = 2, MilieuDeTerrain = 3 }
+
     public class Joueur
     {
         public ICollection<Club> Club { get; set; }
@@ -19,5 +20,7 @@ namespace FIFA_API.Models.LE_CODE_FIRST____
         public Poste PosteJoueur { get; set; } 
         public string Biographie { get; set; } = null!;
         public Photo Photo { get; set; } = null!;
+        
+        public Statistiques Stats { get; set; }
     }
 }
