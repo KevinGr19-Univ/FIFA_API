a1d42a35899bc09a42badd189cffad188da9a4c3
Imene Hidri <Imene.hidri@etu.univ-smb.fr>
2024-03-12 16:59:48 +0100

    Code first : FaqJoueur, Statistiques

---
+++ FIFA_API/Models/LE CODE FIRST !!!/FaqJoueur.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FIFA_API.Models.EntityFramework
+{
+    public abstract class FaqJoueur
+    {
+        public int Id { get; set; }
+
+        public int Idjoueur { get; set; }
+
+        public string Question { get; set; } 
+
+        public string Reponse { get; set; } 
+    }
+}
---
+++ FIFA_API/Models/LE CODE FIRST !!!/Statistiques.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FIFA_API.Models.EntityFramework
+{
+    public abstract class StatistiquesJoueur
+    {
+ 
+        public int Id { get; set; }
+        public int JoueurId { get; set; }
+        public int MatchsJoues { get; set; }
+        public int Titularisations { get; set; }
+        public int MinutesJouees { get; set; }
+        public int Buts { get; set; }
+    }
+}
