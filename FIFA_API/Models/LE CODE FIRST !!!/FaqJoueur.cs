9626477b961bafe094c548c92ab919337540984c
Imene Hidri <Imene.hidri@etu.univ-smb.fr>
2024-03-12 17:24:20 +0100

    .

--- FIFA_API/Models/LE CODE FIRST !!!/FaqJoueur.cs
+++ FIFA_API/Models/LE CODE FIRST !!!/FaqJoueur.cs
@@ -7,7 +7,7 @@ namespace FIFA_API.Models.LE_CODE_FIRST____
     {
         public int Id { get; set; }
 
-        public Joueur joueur { get; set; }
+        public Joueur Joueur { get; set; }
 
         public string Question { get; set; } 
 
