5f4641417e8c265bebc7d164b7baed506694814c
Loïc Chandon <Loic.Chandon@etu.univ-smb.fr>
2024-03-12 17:45:14 +0100

    Utilisateur code first

--- FIFA_API/Models/LE CODE FIRST !!!/Utilisateur.cs
+++ FIFA_API/Models/LE CODE FIRST !!!/Utilisateur.cs
@@ -2,5 +2,18 @@
 {
     public class Utilisateur
     {
+        //Ajouter les roles
+        public enum RoleUtilisateur { Utilisateur = 0, DirecteurDuServiceVentes = 1, MembreDuServiceExpedition = 2, MembreDuServiceCommande = 3}
+        public string StripeId { get; set; }
+        public string Telephone { get; set; }
+        public string Prenom { get; set; }
+        public string Mail { get; set; }
+        public string Surnom { get; set; }
+        public DateTime DateNaissance { get; set; }
+        public string MotDePasse { get; set; }
+        public RoleUtilisateur Role { get; set; }
+        public DateTime DerniereConnexion { get; set; }
+        public DateTime DateVerificationEmail { get; set; }
+        public bool DoubleAuthentification { get; set; }
     }
 }
