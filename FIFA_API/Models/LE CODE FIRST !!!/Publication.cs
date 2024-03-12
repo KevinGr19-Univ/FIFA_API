5eb4ebfc848cd7c08126ff2650bc6669865a1de7
KevinGr19 <kevingr19dev@gmail.com>
2024-03-12 16:53:30 +0100

    Création dossier LE CODE FIRST !!!

---
+++ FIFA_API/Models/LE CODE FIRST !!!/Publication.cs
@@ -0,0 +1,10 @@
+﻿namespace FIFA_API.Models.EntityFramework
+{
+    public abstract class Publication
+    {
+        public string Titre { get; set; }
+        public string Resume { get; set; }
+        public DateTime DatePublication { get; set; }
+        public Photo? Photo { get; set; }
+    }
+}
