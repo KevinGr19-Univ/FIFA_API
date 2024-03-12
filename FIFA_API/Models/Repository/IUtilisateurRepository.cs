f2667cbb70a31cefaf19c3787b93c75318a8a3aa
KevinGr19 <kevingr19dev@gmail.com>
2024-03-12 16:34:47 +0100

    Ajout de IUtilisateurRepository (example de repo)

---
+++ FIFA_API/Models/Repository/IUtilisateurRepository.cs
@@ -0,0 +1,10 @@
+﻿using FIFA_API.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIFA_API.Models.Repository
+{
+    public interface IUtilisateurRepository : IRepository<Utilisateur>
+    {
+        Task<ActionResult<Utilisateur?>> GetByEmailAsync(string email);
+    }
+}
