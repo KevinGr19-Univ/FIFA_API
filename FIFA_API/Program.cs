9857ba515855d00b7cf9507ee3fdd61bec1b2578
EliXorZz <dylan.battig@gmail.com>
2024-03-13 15:38:04 +0100

    DB Context setup service

--- FIFA_API/Program.cs
+++ FIFA_API/Program.cs
@@ -1,6 +1,12 @@
+using FIFA_API.Models.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<FifaDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("FifaDBContext"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
