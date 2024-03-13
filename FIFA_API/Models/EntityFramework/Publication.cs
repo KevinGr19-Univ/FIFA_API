90cd39b4d066a79b1e98ae7f871948e21bd2c135
KevinGr19 <kevingr19dev@gmail.com>
2024-03-13 11:40:50 +0100

    Ajout des data annotations de check

--- FIFA_API/Models/EntityFramework/Publication.cs
+++ FIFA_API/Models/EntityFramework/Publication.cs
@@ -6,14 +6,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 	[Table("t_g_publication_pub")]
     public abstract partial class Publication
     {
+        public const int MAX_TITRE_LENGTH = 200;
+        public const int MAX_RESUME_LENGTH = 600;
+
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("pub_id")]
         public int Id { get; set; }
 
         [Column("pub_titre")]
+        [StringLength(MAX_TITRE_LENGTH, ErrorMessage = "Le titre ne doit pas dépasser 200 caractères.")]
         public string Titre { get; set; }
 
 		[Column("pub_resume")]
+        [StringLength(MAX_RESUME_LENGTH, ErrorMessage = "Le résumé ne doit pas dépasser 600 caractères.")]
         public string Resume { get; set; }
 
 		[Column("pub_datepublication")]
