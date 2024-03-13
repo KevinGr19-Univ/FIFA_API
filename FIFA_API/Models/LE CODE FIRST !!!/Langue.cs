bb567f0aa9cc3eeea1a8ec8c39c52a769b6fffb1
Loïc Chandon <Loic.Chandon@etu.univ-smb.fr>
2024-03-13 11:02:36 +0100

    Langue + Utilisateur

---
+++ FIFA_API/Models/LE CODE FIRST !!!/Langue.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+﻿namespace FIFA_API.Models.LE_CODE_FIRST____
+{
+	[Table("t_e_langue_lan")]
+    public class Langue
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("lan_id")]
+        public int Id { get; set; }
+
+        [Column("lan_nom")]
+        public string Nom { get; set; }
+
+    }
+}
