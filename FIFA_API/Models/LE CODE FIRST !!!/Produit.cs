842708f41a4370c6cb82e59fc07d934c5622ae5f
KevinGr19 <kevingr19dev@gmail.com>
2024-03-13 10:59:24 +0100

    Ajout Id FK

--- FIFA_API/Models/LE CODE FIRST !!!/Produit.cs
+++ FIFA_API/Models/LE CODE FIRST !!!/Produit.cs
@@ -11,9 +11,6 @@ namespace FIFA_API.Models.LE_CODE_FIRST____
         [Key]
         [Column("prd_id")]
         public int Id { get; set; }
-        public Competition Competition { get; set; }
-        public Nation Nation { get; set; }
-        public Genre Genre { get; set; }
 
 		[Column("prd_titre")]
         public string Titre { get; set; }
@@ -21,6 +18,23 @@ namespace FIFA_API.Models.LE_CODE_FIRST____
 		[Column("prd_description")]
         public string Description { get; set; }
 
+        [Column("cmp_id")]
+        public int IdCompetition { get; set; }
+
+        [Column("nat_id")]
+        public int IdNation { get; set; }
+
+        [Column("gen_id")]
+        public int IdGenre { get; set; }
+
+        [ForeignKey(nameof(IdCompetition))]
+        public Competition Competition { get; set; }
+
+        [ForeignKey(nameof(IdNation))]
+        public Nation Nation { get; set; }
+
+        [ForeignKey(nameof(IdGenre))]
+        public Genre Genre { get; set; }
 
         public ICollection<Produit> Associes { get; set; }
         public ICollection<Couleur> Couleurs { get; set; }
