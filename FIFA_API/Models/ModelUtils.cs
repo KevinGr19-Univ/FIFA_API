26c9372aff29ad33553cd8da85ba8113676b5a34
Loïc Chandon <Loic.Chandon@etu.univ-smb.fr>
2024-03-13 11:55:25 +0100

    ok I PUSH UP (fin de matinée, TypeLivraison -> VoteUtilisateur) + ajout regex passwd

--- FIFA_API/Models/ModelUtils.cs
+++ FIFA_API/Models/ModelUtils.cs
@@ -5,5 +5,6 @@
         public const string REGEX_CODEPOSTAL = "^([0-9]{2}|2[AB])[0-9]{3}$"; // A tester
         public const string REGEX_HEXACOLOR = "^[0-9A-F]{6}$"; // A tester
         public const string REGEX_TELEPHONE = "^0[1-9][0-9]{8}$";
+        public const string REGEX_PASSWORD = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{12,20})$";
     }
 }
