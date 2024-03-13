ed1417a8023f25c49a11466066cbe7d8566e915e
KevinGr19 <kevingr19dev@gmail.com>
2024-03-13 16:01:44 +0100

    Resolution conflits

--- FIFA_API/Models/EntityFramework/VarianteCouleurProduit.cs
+++ FIFA_API/Models/EntityFramework/VarianteCouleurProduit.cs
@@ -11,17 +11,21 @@ namespace FIFA_API.Models.EntityFramework
             ImageUrls = new List<string>();
         }
 
-        [Key, Column("prd_id", Order = 0)]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("vcp_id")]
+        public int Id { get; set; }
+
+        [Column("prd_id", Order = 0)]
         public int IdProduit { get; set; }
 
-        [Key, Column("col_id", Order = 1)]
+        [Column("col_id", Order = 1)]
         public int IdCouleur { get; set; }
 
 		[Column("vcp_prix")]
         public decimal Prix { get; set; }
 
-        [Column("vcp_images")]
-        public IList<string> ImageUrls { get; set; }
+        [Column("vcp_images", TypeName = "varchar[]")]
+        public List<string> ImageUrls { get; set; }
 
         [ForeignKey(nameof(IdProduit))]
         public Produit Produit { get; set; }
