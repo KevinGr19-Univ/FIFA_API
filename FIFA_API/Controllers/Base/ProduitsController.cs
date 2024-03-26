b9050bffdd04dc1e0bcb3634cdb8909110bccc30
KevinGr19 <kevin.grandjean@etu.univ-smb.fr>
2024-03-26 15:34:44 +0100

    Ajout Controller Commandes et Produits (sans pagination)

---
+++ FIFA_API/Controllers/Base/ProduitsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models;
+using Microsoft.AspNetCore.Authorization;
+using FIFA_API.Utils;
+
+namespace FIFA_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public partial class ProduitsController : ControllerBase
+    {
+        private const string MANAGER_POLICY = Policies.DirecteurVente;
+
+        private readonly FifaDbContext _context;
+
+        public ProduitsController(FifaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Produits
+        [HttpGet]
+        [Authorize(Policy = MANAGER_POLICY)]
+        public async Task<ActionResult<IEnumerable<Produit>>> GetProduits()
+        {
+            return await _context.Produits.ToListAsync();
+        }
+
+        // GET: api/Produits/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produit>> GetProduit(int id)
+        {
+            var produit = await _context.Produits.FindAsync(id);
+
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            return produit;
+        }
+
+        // PUT: api/Produits/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [Authorize(Policy = MANAGER_POLICY)]
+        public async Task<IActionResult> PutProduit(int id, Produit produit)
+        {
+            if (id != produit.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await _context.UpdateEntity(produit);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProduitExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Produits
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Authorize(Policy = MANAGER_POLICY)]
+        public async Task<ActionResult<Produit>> PostProduit(Produit produit)
+        {
+            await _context.Produits.AddAsync(produit);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduit", new { id = produit.Id }, produit);
+        }
+
+        // DELETE: api/Produits/5
+        [HttpDelete("{id}")]
+        [Authorize(Policy = MANAGER_POLICY)]
+        public async Task<IActionResult> DeleteProduit(int id)
+        {
+            var produit = await _context.Produits.FindAsync(id);
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produits.Remove(produit);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProduitExists(int id)
+        {
+            return (_context.Produits?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ FIFA_API/Controllers/ProduitsController.Part.cs
@@ -0,0 +1,55 @@
+﻿using FIFA_API.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIFA_API.Controllers
+{
+    public partial class ProduitsController
+    {
+        [HttpGet("Search")]
+        public async Task<ActionResult<IEnumerable<Produit>>> SearchProduits(
+            [FromQuery] string? q,
+            [FromQuery] int[] categories,
+            [FromQuery] int[] competitions,
+            [FromQuery] int[] genres,
+            [FromQuery] int[] nations,
+            [FromQuery] int[] couleurs,
+            [FromQuery] int[] tailles,
+            [FromQuery] bool? desc,
+            [FromQuery] int? page)
+        {
+            IQueryable<Produit> query = _context.Produits
+                .Include(p => p.Variantes)
+                .Include(p => p.Tailles);
+
+            if (categories.Length > 0)
+                query = query.Where(p => categories.Contains(p.IdCategorieProduit));
+
+            if (competitions.Length > 0)
+                query = query.Where(p => competitions.Contains(p.IdCompetition));
+
+            if (genres.Length > 0)
+                query = query.Where(p => genres.Contains(p.IdGenre));
+
+            if (nations.Length > 0)
+                query = query.Where(p => nations.Contains(p.IdNation));
+
+            if (couleurs.Length > 0)
+                query = query.Where(p => p.Variantes.Any(v => couleurs.Contains(v.IdCouleur)));
+
+            if (tailles.Length > 0)
+                query = query.Where(p => p.Tailles.Any(t => tailles.Contains(t.Id)));
+
+            if(q is not null)
+                query = query.Where(p => p.Titre.ToLower().Contains(q.ToLower()));
+
+            if (desc == true)
+                query = query.OrderBy(p => p.Variantes.Select(v => v.Prix).Min());
+
+            else if (desc == false)
+                query = query.OrderByDescending(p => p.Variantes.Select(v => v.Prix).Min());
+
+            return Ok(await query.ToListAsync());
+        }
+    }
+}
