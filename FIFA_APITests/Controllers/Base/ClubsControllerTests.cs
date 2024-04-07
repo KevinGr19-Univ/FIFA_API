569eaf6184bd8e25cc299647f2490727b5cc63c5
KevinGr19 <kevingr19dev@gmail.com>
2024-04-07 22:05:06 +0200

    Init branche tests

---
+++ FIFA_APITests/Controllers/Base/ClubsControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FIFA_API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFA_API.Controllers.Tests
+{
+    [TestClass()]
+    public class ClubsControllerTests
+    {
+        [TestMethod()]
+        public void ClubsControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetClubsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetClubTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutClubTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostClubTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteClubTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
